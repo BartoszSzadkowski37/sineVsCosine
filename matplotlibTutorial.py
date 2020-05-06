4595919f612c4ac826d3ff54297589266dd1f58c
Bartosz Szadkowski <bartoszszadkowski@MacBook-Air-Bartosz.local>
2020-05-06 13:01:47 +0200

    Sine and Cosine curves are ok, finish diagram, check tutorial

--- matplotlibTutorial.py
+++ matplotlibTutorial.py
@@ -1,19 +1,27 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
-#plt.style.use('fivethirtyeight') # Adding style
-plt.xkcd() # Adding comics style
+plt.style.use('fivethirtyeight') # Adding style
+#plt.xkcd() # Adding comics style
 ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35] # X axis
+
+x_indexes = np.arange(len(ages_x))
+
+width = 0.25
 
 dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752] # Y curve
 
-plt.plot(ages_x, dev_y, color='#444444', linestyle='--', marker='.', label='All Devs') # Add axis and curve to the diagram
+plt.bar(x_indexes - width, dev_y, width=width, color='#444444', label='All Devs') # Add axises to the diagram
+
 py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
 
-plt.plot(ages_x, py_dev_y, color='#5a7d9a', marker='o', label='Python') # Add axis and curve to the diagram
+plt.bar(x_indexes, py_dev_y, width=width, color='#5a7d9a', label='Python') # Add axises  to the diagram
 
 js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
 
-plt.plot(ages_x, js_dev_y, color='#adad3b', label='JavaScript') # Add axis and curve to the diagram
+plt.bar(x_indexes + width, js_dev_y, width=width, color='#adad3b', label='JavaScript') # Add axises to the diagram
+
+plt.xticks(ticks=x_indexes, labels=ages_x)
 
 plt.xlabel('Ages') # Label for X axis
 plt.ylabel('Median Salary (USD)') # Label for Y axis
--- sineVsCosine.py
+++ sineVsCosine.py
@@ -1,1 +1,16 @@
 # Sine and Cosine generating
+
+from matplotlib import pyplot as plt # Importing module
+import numpy as np
+
+x_axis = np.arange(0, 4*np.pi, 0.1) # Start, stop, step
+y_sine = np.sin(x_axis)
+y_cosine = np.cos(x_axis)
+
+plt.plot(x_axis, y_sine) # Sine curve
+plt.plot(x_axis, y_cosine) # Cosine curve
+plt.show()
+
+degree = '\u00B0'
+
+
